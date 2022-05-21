764a129bc06ac33513d062b3812b3c4af39defa7
“00Amit0” <amit836961@gmail.com>
2022-05-21 09:10:07 +0530

    Feature Branch Push

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            Factorization.Factor();
+            //HarmonicNumber.Harmonic();
+            //PowerOf2.Power();
+            //FlipCoin.FlipTheCoin();
+            //LeapYear.PrintLeapYear();
+            //DemoLoop.PrintForLoop();
+            //Username.PrintUsername();
+            //Username.name = "World";
+            /* int value1 = 6;
+             double value2 = 6.7898;
+             bool value3 = false;
+             char value4 = 'a';
+             string value5 = "amit";
+             Console.WriteLine("Hello World :"+value1);
+             Console.WriteLine("Double value:" + value2);
+             Console.WriteLine("Boolean value:" + value3);
+             Console.WriteLine("Character value:" + value4);
+             Console.WriteLine("String value:" + value5); 
+
+            Console.WriteLine("Please enter the username");
+            string username = Console.ReadLine();
+            Console.WriteLine("Hello " + username + " , How are you ?");*/
+        }
+    }
+}
